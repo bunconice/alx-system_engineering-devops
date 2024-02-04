f2c4af3fccd29a1d07826aa8fe267668f99e6da4
Bunmi Akinola <bunconice@gmail.com>
2024-02-04 22:26:05 +0100

    script that gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# script that gather data from an API
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    task_completed = 0
+    user = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/"
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    param = {"userId": argv[1]}
+    user_response = requests.get(user_url + f"{user}").json()
+    todo_data = requests.get(todo_url, params=param).json()
+    for item in todo_data:
+        if (item["completed"]):
+            task_completed += 1
+    tasks = len(todo_data)
+    print(f"Employee {user_response['name']} is done with tasks", end="")
+    print(f"({task_completed}/{tasks}):")
+    for item in todo_data:
+        print("\t ",format(item['title']))
