5dba9eeb2253d22e2f4ccc021ec7a70172558ba1
Bunmi Akinola <bunconice@gmail.com>
2024-02-05 13:54:32 +0100

    updated

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-"""
-script that gather data from an API
-and export them in JSON format
-"""
+"""Exports data in JSON format"""
 
-import json
-import requests
+from json import dump
+from requests import get
 from sys import argv
 
-if __name__ == "__main__":
-    user_id = argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+if __name__ == '__main__':
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(url)
+    username = response.json().get('username')
+
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
+    tasks = response.json()
+    dictionary = {user_id: []}
+    for task in tasks:
+        dictionary[user_id].append({
+                                    "task": task.get('title'),
+                                    "completed": task.get('completed'),
+                                    "username": username
+                                    })
+    with open('{}.json'.format(user_id), 'w') as file:
+        dump(dictionary, file)
