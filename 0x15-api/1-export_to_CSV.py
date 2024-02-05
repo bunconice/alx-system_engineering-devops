ef6550ec7fb1b0fe021ffe280ce95bdb94286876
Bunmi Akinola <bunconice@gmail.com>
2024-02-05 09:30:26 +0100

    script that exports data in CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that gather data from an API
+and export them in the CSV format
+"""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/"
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    param = {"userId": argv[1]}
+    user_response = requests.get(user_url + f"{user}").json()
+    todo_data = requests.get(todo_url, params=param).json()
+
+    with open("USER_ID.csv", mode="w", newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for item in todo_data:
+            writer.writerow([item["userId"], user_response['name'],
+                             item["completed"], item["title"]])
