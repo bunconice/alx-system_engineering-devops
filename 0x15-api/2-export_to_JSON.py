081cf0e6468b94ffced498c9d8e78d195948311f
Bunmi Akinola <bunconice@gmail.com>
2024-02-05 13:04:08 +0100

    script that export data in  JSON format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that gather data from an API
+and export them in JSON format
+"""
+
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/"
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    param = {"userId": argv[1]}
+    user_response = requests.get(user_url + f"{user}").json()
+    todo_data = requests.get(todo_url, params=param).json()
+    username = user_response["username"]
+    with open("{}.json".format(user), "w") as file:
+        json.dump(
+            {user: [{"task": item['title'],
+                     "completed": item['completed'],
+                     "username": username} for item in todo_data]}, file)
