6728fa2e9e3d255beb5f0bb48a8f3cfb82b3ee1b
Bunmi Akinola <bunconice@gmail.com>
2024-02-13 16:16:33 +0100

    function that queries the Reddit AP

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
+
 """
-Importing requests module
+    Write a function that queries the Reddit
+    API and returns the number of subscribers.
 """
 
-from requests import get
+import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
     """
-    returns the number of subscribers
-    If not a valid subreddit, return 0.
+        Number of suscribers
     """
-    # Check if the subredit is none, Return 0
-    if subreddit is None or subreddit is not isinstance(subreddit, str):
-        return 0
-
-    # Identifiying the user agent
-    userAgent = {' User-agent': 'Google Chrome Version 81.0.4044.129'}
-
-    # Identify the url
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-
-    # Make the requesst
-    response = get(url, head=userAgent)
-
-    # Convert the response intp json format
-    jsonResponse = response.json()
-
-    # Try to get the number of subscribes
     try:
-        return jsonResponse.get('data').get('subscribers')
-    # If you could't get it
-    except exceptions.RequestException as e:
+        base_url = 'https://www.reddit.com/r/'
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        r = requests.get(
+            base_url + '{}/about.json'.format(subreddit), headers=headers)
+        return r.json().get('data').get('subscribers')
+    except:
         return 0
