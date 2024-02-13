3dbc9f0fbce0d4705f7cc1eadb83c17a7d87376a
Bunmi Akinola <bunconice@gmail.com>
2024-02-13 16:19:14 +0100

    function that queries the Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-
-"""
-    Write a function that queries the Reddit
-    API and returns the number of subscribers.
-"""
-
+'''
+    this module contains the function number_of_subscribers
+'''
 import requests
-import sys
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """
-        Number of suscribers
-    """
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
     try:
-        base_url = 'https://www.reddit.com/r/'
-        headers = {'User-Agent': 'Mozilla/5.0'}
-        r = requests.get(
-            base_url + '{}/about.json'.format(subreddit), headers=headers)
-        return r.json().get('data').get('subscribers')
-    except:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
